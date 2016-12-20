b35bfbed685803cfe34027b4bd722d5aca883d80
Sakib Hossain <Sakibhossain.nstu@gmail.com>
2016-12-20 17:33:05 +0600

    Create Problem23.java

---
+++ Problem23.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class P23 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+String S=sc.nextLine();
+System.out.println(S);
+System.out.println(S.toUpperCase());
+System.out.println(S.toLowerCase());
+	}
+
+}
